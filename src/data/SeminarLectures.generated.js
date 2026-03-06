06a07398ceda1eff0862b4a3789b1d15f673c702
codebasehs-lang <codebase.hs@gmail.com>
2026-03-06 16:51:04 +0530

    audio lecture added.

---
+++ src/data/SeminarLectures.generated.js
@@ -0,0 +1,123 @@
+module.exports = {
+  "id": 4,
+  "playlistName": "ब्रहमचारी क्लास",
+  "category": [
+    "ब्रहमचारी क्लास"
+  ],
+  "description": "ब्रहमचारी क्लास Hindi audio lectures",
+  "language": "Hindi",
+  "location": "इस्कॉन पुणे",
+  "icon": "",
+  "audios": [
+    {
+      "id": 327,
+      "title": "ब्रहमचारी क्लास – ययाति महाराज की अनुभूति",
+      "audioUrl": "https://audio.iskcondesiretree.com/02_-_ISKCON_Swamis/ISKCON_Swamis_-_D_to_P/His_Holiness_Haladhar_Swami/Hindi_Lectures/Brahmachari_Class/Haladhar_Sw_Brahmachari_Class_Hindi_-_Realizations_of_Yayati_Maharaj_-_2003-06-17_Camp_Pune.mp3",
+      "duration": "1:36:17",
+      "date": "2003-06-17",
+      "location": "इस्कॉन पुणे कैम्प",
+      "language": "Hindi",
+      "hasTranscription": false,
+      "transcription": ""
+    },
+    {
+      "id": 328,
+      "title": "ब्रहमचारी क्लास",
+      "audioUrl": "https://audio.iskcondesiretree.com/02_-_ISKCON_Swamis/ISKCON_Swamis_-_D_to_P/His_Holiness_Haladhar_Swami/Hindi_Lectures/Brahmachari_Class/Haladhar_Sw_Brahmachari_Class_Hindi_-_2004-06-17_Camp_Pune.mp3",
+      "duration": "0:52:31",
+      "date": "2004-06-17",
+      "location": "इस्कॉन पुणे कैम्प",
+      "language": "Hindi",
+      "hasTranscription": false,
+      "transcription": ""
+    },
+    {
+      "id": 329,
+      "title": "ब्रहमचारी क्लास",
+      "audioUrl": "https://audio.iskcondesiretree.com/02_-_ISKCON_Swamis/ISKCON_Swamis_-_D_to_P/His_Holiness_Haladhar_Swami/Hindi_Lectures/Brahmachari_Class/Haladhar_Sw_Various_Hindi_-_Brahmchari_Class_-_2018-06-26_ISKCON_Uttam_Nagar.mp3",
+      "duration": "1:16:24",
+      "date": "2018-06-26",
+      "location": "इस्कॉन उत्तम नगर पुणे",
+      "language": "Hindi",
+      "hasTranscription": false,
+      "transcription": ""
+    },
+    {
+      "id": 330,
+      "title": "ब्रहमचारी क्लास – ब्रह्मचारियों के लिए कठिन चुनोतियाँ",
+      "audioUrl": "https://audio.iskcondesiretree.com/02_-_ISKCON_Swamis/ISKCON_Swamis_-_D_to_P/His_Holiness_Haladhar_Swami/Hindi_Lectures/Brahmachari_Class/Haladhar_Sw_Brahmachari_Class_Hindi_-_The_Strong_Challanges_to_Bramhacharis_-_2022-03_Mayapur.mp3",
+      "duration": "1:22:09",
+      "date": "2022-03",
+      "location": "इस्कॉन मायापुर",
+      "language": "Hindi",
+      "hasTranscription": false,
+      "transcription": ""
+    },
+    {
+      "id": 331,
+      "title": "ब्रहमचारी क्लास – एक कठोर ब्रह्मचर्य जीवन",
+      "audioUrl": "https://audio.iskcondesiretree.com/02_-_ISKCON_Swamis/ISKCON_Swamis_-_D_to_P/His_Holiness_Haladhar_Swami/Hindi_Lectures/Brahmachari_Class/Haladhar_Sw_Brahmachari_Class_Hindi_-_A_rigid_Bramchari_Life_-_2022-12-14_Nasik.mp3",
+      "duration": "1:22:32",
+      "date": "2022-12-14",
+      "location": "इस्कॉन नासिक",
+      "language": "Hindi",
+      "hasTranscription": false,
+      "transcription": ""
+    },
+    {
+      "id": 332,
+      "title": "ब्रहमचारी क्लास - भाग-01",
+      "audioUrl": "https://audio.iskcondesiretree.com/02_-_ISKCON_Swamis/ISKCON_Swamis_-_D_to_P/His_Holiness_Haladhar_Swami/Hindi_Lectures/Brahmachari_Class/Haladhar_Sw_Brahmachari_Class_Hindi_-_Day-01_-_2023-05-07_Kanpur.mp3",
+      "duration": "1:52:52",
+      "date": "2023-05-07",
+      "location": "इस्कॉन कानपुर",
+      "language": "Hindi",
+      "hasTranscription": false,
+      "transcription": ""
+    },
+    {
+      "id": 333,
+      "title": "ब्रहमचारी क्लास – प्रशन्नो/उत्तरी",
+      "audioUrl": "https://audio.iskcondesiretree.com/02_-_ISKCON_Swamis/ISKCON_Swamis_-_D_to_P/His_Holiness_Haladhar_Swami/Hindi_Lectures/Brahmachari_Class/Haladhar_Sw_Brahmachari_Class_Hindi_-_Q%26A_-_2023-05-07_Kanpur.mp3",
+      "duration": "1:32:52",
+      "date": "2023-05-07",
+      "location": "इस्कॉन कानपुर",
+      "language": "Hindi",
+      "hasTranscription": false,
+      "transcription": ""
+    },
+    {
+      "id": 334,
+      "title": "ब्रहमचारी क्लास – ब्रह्मचारियों के लिए आहार भाग-02",
+      "audioUrl": "https://audio.iskcondesiretree.com/02_-_ISKCON_Swamis/ISKCON_Swamis_-_D_to_P/His_Holiness_Haladhar_Swami/Hindi_Lectures/Brahmachari_Class/Haladhar_Sw_Brahmachari_Class_Hindi_-_Diet_for_Bramhacharis_Day-02_-_2023-05-08_Kanpur.mp3",
+      "duration": "2:36:32",
+      "date": "2023-05-08",
+      "location": "इस्कॉन कानपुर",
+      "language": "Hindi",
+      "hasTranscription": false,
+      "transcription": ""
+    },
+    {
+      "id": 335,
+      "title": "ब्रहमचारी क्लास – ब्रह्मचारियों के लिए सुनहरे नियम",
+      "audioUrl": "https://audio.iskcondesiretree.com/02_-_ISKCON_Swamis/ISKCON_Swamis_-_D_to_P/His_Holiness_Haladhar_Swami/Hindi_Lectures/Brahmachari_Class/Haladhar_Sw_Brahmachari_Class_Hindi_-_Golden_Rules_for_Bramhacharis_-_Pune.mp3",
+      "duration": "2:10:06",
+      "date": "",
+      "location": "इस्कॉन पुणे",
+      "language": "Hindi",
+      "hasTranscription": false,
+      "transcription": ""
+    },
+    {
+      "id": 336,
+      "title": "ब्रहमचारी क्लास – प्रशन्नो/उत्तरी ब्रह्मचारी जीवन के लिए भाग-03",
+      "audioUrl": "https://audio.iskcondesiretree.com/02_-_ISKCON_Swamis/ISKCON_Swamis_-_D_to_P/His_Holiness_Haladhar_Swami/Hindi_Lectures/Brahmachari_Class/Haladhar_Sw_Brahmachari_Class_Hindi_-_Q%26A_on_Bramhachari_Life_Day-03_-_2023-05-09_Kanpur.mp3",
+      "duration": "2:46:58",
+      "date": "2023-05-09",
+      "location": "इस्कॉन कानपुर",
+      "language": "Hindi",
+      "hasTranscription": false,
+      "transcription": ""
+    }
+  ]
+};
